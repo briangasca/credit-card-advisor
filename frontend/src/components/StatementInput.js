cb3a8f0e7ea351bf2a3beb1a4433d09a596544c2
Brian Gasca <bgasca@ucsd.edu>
2025-10-15 22:29:23 -0700

    set up simple document submission

--- frontend/src/components/StatementInput.js
+++ frontend/src/components/StatementInput.js
@@ -1,11 +1,34 @@
 import './StatementInput.css'
+import { useState } from 'react';
 
 export default function StatementInput() {
+    const [inputFile, setInputFile] = useState();
+    const [submissionFeedback, setSubmissionFeedback] = useState('')
+    
+    
+    const handleSubmitInput = (e) => {
+        e.preventDefault();
+
+        if (!inputFile) {
+            setSubmissionFeedback('Please select a file before submitting.')
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append("statement", inputFile);
+        setSubmissionFeedback('Successfully submitted! Loading...');
+    }
+
+    const handleSubmitInputChange = (e) => {
+        setInputFile(e.target.files[0]);
+    }
+
     return (
         <div className='statement-input-container'>
             <h2 className='statement-input-header'>Input your statement as a .pdf here:</h2>
-            <input type='file' className='statement-input-input'></input>
-            <button className='statement-input-submit'> Submit Statement</button>
+            <input type='file' className='statement-input-input' onChange={handleSubmitInputChange}></input>
+            <button className='statement-input-submit' onClick={handleSubmitInput}> Submit Statement</button>
+            <p className="statement-input-submission-feedback">{submissionFeedback}</p>
         </div>
     )
 } 
